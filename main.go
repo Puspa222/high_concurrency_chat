623c1e9a09d608ac636b529865b9bd27e7d5a805
puspa222 <pusparegmi11b@gmail.com>
2025-12-25 16:44:41 +0545

    Refactor main function to improve chat server startup and shutdown logging

--- main.go
+++ main.go
@@ -3,8 +3,11 @@ package main
 import "fmt"
 
 func main() {
+	for i := 1; i < 10; i++ {
+		fmt.Println("--------------------------------------------------------------------")
+	}
 	fmt.Printf("Starting chat server...\n")
-	done := make(chan struct{})
-	<-done
-	// fmt.Println("Closing chat server...")
+	// done := make(chan struct{})
+	// <-done
+	fmt.Println("Closing chat server...")
 }
